a84b2b1caed3b718cefd52f1508ca6fe27750ce5
Jean Fan <jeanfan@fas.harvard.edu>
2017-10-10 17:31:01 -0400

    Add mudan functions

---
+++ R/mudan.R
@@ -0,0 +1,454 @@
+##' Filter counts matrix
+##'
+##' Filter counts matrix based on gene and cell requirements
+##'
+##' @param counts Read count matrix. The rows correspond to genes, columns correspond to individual cells
+##' @param min.lib.size Minimum number of genes detected in a cell. Cells with fewer genes will be removed (default: 1.8e3)
+##' @param min.reads Minimum number of reads per gene. Genes with fewer reads will be removed (default: 10)
+##' @param min.detected Minimum number of cells a gene must be seen in. Genes not seen in a sufficient number of cells will be removed (default: 5)
+##'
+##' @return a filtered read count matrix
+##'
+##' @examples {
+##' data(pbmcA)
+##' dim(pbmcA)
+##' mat <- cleanCounts(pbmcA)
+##' dim(mat)
+##' }
+##'
+##' @export
+##'
+cleanCounts <- function(counts, min.lib.size = 1.8e3, min.reads = 10, min.detected = 5) {
+    ## filter out low-gene cells
+    counts <- counts[, colSums(counts>0)>min.lib.size]
+    ## remove genes that don't have many reads
+    counts <- counts[rowSums(counts)>min.reads, ]
+    ## remove genes that are not seen in a sufficient number of cells
+    counts <- counts[rowSums(counts>0)>min.detected, ]
+    return(counts)
+}
+
+
+##' Normalize gene expression variance relative to transcriptome-wide expectations
+##' (Modified from SCDE/PAGODA2 code; now uses Rfast)
+##'
+##' Normalizes gene expression magnitudes to with respect to its ratio to the
+##' transcriptome-wide expectation as determined by local regression on expression magnitude
+##'
+##' @param counts Read count matrix. The rows correspond to genes, columns correspond to individual cells
+##' @param gam.k Generalized additive model parameter; the dimension of the basis used to represent the smooth term (default: 5)
+##' @param alpha Significance threshold (default: 0.05)
+##' @param plot Whether to plot the results (default: FALSE)
+##' @param use.unadjusted.pvals If true, will apply BH correction (default: FALSE)
+##' @param do.par Whether to adjust par for plotting if plotting (default: TRUE)
+##' @param max.adjusted.variance Ceiling on maximum variance after normalization to prevent infinites (default: 1e3)
+##' @param min.adjusted.variance Floor on minimum variance after normalization (default: 1e-3)
+##' @param verbose Verbosity (default: TRUE)
+##' @param details If true, will return data frame of normalization parameters. Else will return normalized matrix.(default: FALSE)
+##'
+##' @return If details is true, will return data frame of normalization parameters. Else will return normalized matrix.
+##'
+##' @examples {
+##' data(pbmcA)
+##' mat <- cleanCounts(pbmcA)
+##' mat <- normalizeVariance(mat)
+##' }
+##'
+##' @export
+##'
+normalizeVariance <- function(mat, gam.k=5, alpha=0.05, plot=FALSE, use.unadjusted.pvals=FALSE, do.par=TRUE, max.adjusted.variance=1e3, min.adjusted.variance=1e-3, verbose=TRUE, details=FALSE) {
+    mat <- t(mat) ## make rows as cells, cols as genes
+
+    if(verbose) {
+        print("Calculating variance fit ...")
+    }
+    dfm <- log(Rfast::colmeans(mat))
+    dfv <- log(Rfast::colVars(mat))
+    names(dfm) <- names(dfv) <- colnames(mat)
+    df <- data.frame(m=dfm, v=dfv)
+
+    vi <- which(is.finite(dfv))
+
+    if(length(vi)<gam.k*1.5) { gam.k=1 } ## too few genes
+
+    if(gam.k<2) {
+        if(verbose) {
+            print("Using lm ...")
+        }
+        m <- lm(v ~ m, data = df[vi,])
+    } else {
+        if(verbose) {
+            print("Using gam...")
+        }
+        m <- mgcv::gam(v ~ s(m, k = gam.k), data = df[vi,])
+    }
+    df$res <- -Inf;  df$res[vi] <- resid(m,type='response')
+    n.cells <- ncol(mat)
+    n.obs <- nrow(mat)
+    df$lp <- as.numeric(pf(exp(df$res),n.obs,n.obs,lower.tail=F,log.p=T))
+    df$lpa <- bh.adjust(df$lp,log=TRUE)
+    df$qv <- as.numeric(qchisq(df$lp, n.cells-1, lower.tail = FALSE,log.p=TRUE)/n.cells)
+
+    if(use.unadjusted.pvals) {
+        ods <- which(df$lp<log(alpha))
+    } else {
+        ods <- which(df$lpa<log(alpha))
+    }
+    if(verbose) {
+        print(paste0(length(ods), ' overdispersed genes ... ' ))
+    }
+
+    df$gsf <- geneScaleFactors <- sqrt(pmax(min.adjusted.variance,pmin(max.adjusted.variance,df$qv))/exp(df$v));
+    df$gsf[!is.finite(df$gsf)] <- 0;
+
+    if(plot) {
+        if(do.par) {
+            par(mfrow=c(1,2), mar = c(3.5,3.5,2.0,0.5), mgp = c(2,0.65,0), cex = 1.0);
+        }
+        smoothScatter(df$m,df$v,main='',xlab='log10[ magnitude ]',ylab='log10[ variance ]')
+        grid <- seq(min(df$m[vi]),max(df$m[vi]),length.out=1000)
+        lines(grid,predict(m,newdata=data.frame(m=grid)),col="blue")
+        if(length(ods)>0) {
+            points(df$m[ods],df$v[ods],pch='.',col=2,cex=1)
+        }
+        smoothScatter(df$m[vi],df$qv[vi],xlab='log10[ magnitude ]',ylab='',main='adjusted')
+        abline(h=1,lty=2,col=8)
+        if(is.finite(max.adjusted.variance)) { abline(h=max.adjusted.variance,lty=2,col=1) }
+        points(df$m[ods],df$qv[ods],col=2,pch='.')
+    }
+
+    if(!details) {
+        ## variance normalize
+        norm.mat <- t(mat)*df$gsf
+        return(norm.mat)
+    }
+    else {
+        ## return normalization factor
+        return(df)
+    }
+}
+## BH P-value adjustment with a log option
+bh.adjust <- function(x, log = FALSE) {
+    nai <- which(!is.na(x))
+    ox <- x
+    x <- x[nai]
+    id <- order(x, decreasing = FALSE)
+    if(log) {
+        q <- x[id] + log(length(x)/seq_along(x))
+    } else {
+        q <- x[id]*length(x)/seq_along(x)
+    }
+    a <- rev(cummin(rev(q)))[order(id)]
+    ox[nai] <- a
+    ox
+}
+
+
+##' Dimensionality reduction by PCA
+##'
+##' Dimensionality reduction using PCA by computing principal components using highly variable genes
+##'
+##' @param mat Variance normalized gene expression matrix.
+##' @param nGenes Number of most variable genes. (default: 1000)
+##' @param nPcs Number of principal components. (default: 100)
+##'
+##' @return Matrix with columns as cells and rows as principal component eigenvectors.
+##'
+##' @examples {
+##' data(pbmcA)
+##' mat <- cleanCounts(pbmcA)
+##' mat <- normalizeVariance(mat)
+##' pcs <- getPcs(mat)
+##' }
+##'
+##' @export
+##'
+getPcs <- function(mat, nGenes = min(nrow(mat), 1000), nPcs = 100) {
+
+    ## get variable genes
+    vi <- Rfast::rowVars(mat)
+    names(vi) <- rownames(mat)
+    vgenes <- names(sort(vi, decreasing=TRUE)[seq_len(nGenes)])
+    mat <- mat[vgenes,]
+
+    ## get PCs
+    pcs <- fastPca(t(mat), nPcs)
+    m <- t(pcs$l)
+    colnames(m) <- colnames(mat)
+    rownames(m) <- paste0('PC', seq_len(nPcs))
+
+    return(m)
+}
+fastPca <- function(m, nPcs=2, tol=1e-10, scale=FALSE, center=FALSE, transpose=FALSE) {
+    ## note transpose is meant to speed up calculations when neither scaling nor centering is required
+    if(transpose) {
+        if(center) {
+            m <- m-Rfast::rowmeans(m)
+        }
+        if(scale) {
+            m <- m/sqrt(Rfast::rowsums(m*m))
+        }
+        a <- irlba::irlba(tcrossprod(m)/(ncol(m)-1), nu=0, nv=nPcs, tol=tol)
+        a$l <- t(t(a$v) %*% m)
+    } else {
+        if(scale||center) {
+            m <- scale(m, scale=scale, center=center)
+        }
+        a <- irlba::irlba(crossprod(m)/(nrow(m)-1), nu=0, nv=nPcs, tol=tol)
+        a$l <- m %*% a$v
+    }
+    return(a)
+}
+
+
+##' Get approximate nearest neighbors
+##'
+##' Group cells into clusters based on approximate nearest neighbors
+##'
+##' @param mat Matrix of cells as columns. Features as rows (such as PCs).
+##' @param k K-nearest neighbor parameter.
+##' @param method Community detection method from igraph. (default: igraph::cluster_walktrap)
+##' @param verbose Verbosity (default: TRUE)
+##'
+##' @return Vector of community annotations
+##'
+##' @examples {
+##' data(pbmcA)
+##' mat <- cleanCounts(pbmcA)
+##' mat <- normalizeVariance(mat)
+##' pcs <- getPcs(mat)
+##' com <- getKnnMembership(pcs, k=30)
+##' }
+##'
+##' @export
+##'
+getKnnMembership <- function(mat, k, method=igraph::cluster_walktrap, verbose=TRUE) {
+    if(verbose) {
+        print("finding approximate nearest neighbors ...")
+    }
+    knn <- RANN::nn2(t(mat), k=k)[[1]]
+
+    ## convert to adjacency matrix
+    adj <- matrix(0, ncol(mat), ncol(mat))
+    rownames(adj) <- colnames(adj) <- colnames(mat)
+    invisible(lapply(seq_len(ncol(mat)), function(i) {
+        adj[i,colnames(mat)[knn[i,]]] <<- 1
+    }))
+
+    ## convert to graph for clustering
+    if(verbose) {
+        print("calculating clustering ...")
+    }
+    g <- igraph::graph.adjacency(adj, mode="undirected")
+    g <- simplify(g)
+    km <- method(g)
+    if(verbose) {
+        print(paste0('graph modularity: ', modularity(km)))
+    }
+
+    ## community membership
+    com <- km$membership
+    names(com) <- km$names
+    if(verbose) {
+        print("identifying cluster membership ...")
+        print(table(com))
+    }
+    return(com)
+}
+
+
+##' Linear discriminant analysis model
+##'
+##' Identifies components that maximally discriminate among groups using a linear discriminant analysis model
+##'
+##' @param mat Expression matrix with cells as columns, transferable features such as genes as rows.
+##' @param com Community annotations
+##' @param verbose Verbosity (default: TRUE)
+##' @param retest Whether to retest model for accuracy
+##'
+##' @return LDA model
+##'
+##' @examples {
+##' data(pbmcA)
+##' mat <- cleanCounts(pbmcA)
+##' mat <- normalizeVariance(mat)
+##' pcs <- getPcs(mat)
+##' com <- getKnnMembership(pcs, k=30)
+##' model <- modelLda(mat, com)
+##' }
+##'
+##' @export
+##'
+modelLda <- function(mat, com, verbose=TRUE, retest=TRUE) {
+    ## make data frame
+    df <- data.frame(celltype=com, as.matrix(t(mat)))
+
+    if(verbose) {
+        print("calculating LDA ...")
+    }
+    model <- MASS::lda(celltype ~ ., data=df)
+
+    if(retest) {
+        ## predict our data based on model
+        model.output <- predict(model, df)
+        if(verbose) {
+            print("LDA prediction accuracy ...")
+            print(table(model.output$class==com))
+        }
+    }
+
+    return(model)
+}
+
+
+##' Run tSNE on LDs from model
+##'
+##' @export
+##'
+tsneLda <- function(mat, model, com, perplexity=30, verbose=TRUE, plot=TRUE, do.par=TRUE, ncores=10, ...) {
+    if(verbose) {
+        print('Running LDA models ...')
+    }
+
+    ## compute LDs
+    if(class(model)=='lda') {
+        ##reduction <- t(mat[rownames(model$scaling),]) %*% model$scaling
+        reduction <- predict(model, data.frame(t(as.matrix(mat))))$x
+    }
+    if(class(model)=='list') {
+        reduction <- do.call(cbind, lapply(model, function(m) {
+                           reduction <- predict(m, data.frame(t(as.matrix(mat))))$x
+                       }))
+    }
+
+    if(verbose) {
+        print(paste0("Running Rtsne with perplexity ", perplexity))
+    }
+
+    ## tSNE
+    d <- dist(reduction)
+    emb <- Rtsne::Rtsne(d, is_distance=TRUE, perplexity=perplexity, verbose=verbose, num_threads=ncores)$Y
+    rownames(emb) <- labels(d)
+
+    if(plot) {
+        if(do.par) {
+            par(mar = c(0.5,0.5,2.0,0.5), mgp = c(2,0.65,0), cex = 1.0);
+        }
+        plotEmbedding(emb, groups=com, ...)
+    }
+
+    return(emb)
+}
+
+
+##' Plot 2D embedding
+##'
+##' @export
+##'
+plotEmbedding <- function(emb, groups=NULL, colors=NULL, do.par=TRUE, cex=0.6, alpha=0.4, gradientPalette=NULL, zlim=NULL, s=1, v=0.8, min.group.size=1, show.legend=FALSE, mark.clusters=FALSE, mark.cluster.cex=2, shuffle.colors=F, legend.x='topright', gradient.range.quantile=0.95, verbose=TRUE, unclassified.cell.color='gray70', group.level.colors=NULL, ...) {
+
+    if(!is.null(colors)) {
+        ## use clusters information
+        if(!all(rownames(emb) %in% names(colors))) { warning("provided cluster vector doesn't list colors for all of the cells; unmatched cells will be shown in gray. ")}
+        if(all(areColors(colors))) {
+            if(verbose) cat("using supplied colors as is\n")
+            cols <- colors[match(rownames(emb),names(colors))]; cols[is.na(cols)] <- unclassified.cell.color;
+        } else {
+            if(is.numeric(colors)) { # treat as a gradient
+                if(verbose) cat("treating colors as a gradient")
+                if(is.null(gradientPalette)) { # set up default gradients
+                    if(all(sign(colors)>=0)) {
+                        gradientPalette <- colorRampPalette(c('gray80','red'), space = "Lab")(1024)
+                    } else {
+                        gradientPalette <- colorRampPalette(c("blue", "grey70", "red"), space = "Lab")(1024)
+                    }
+                }
+                if(is.null(zlim)) { # set up value limits
+                    if(all(sign(colors)>=0)) {
+                        zlim <- as.numeric(quantile(colors,p=c(1-gradient.range.quantile,gradient.range.quantile)))
+                        if(diff(zlim)==0) {
+                            zlim <- as.numeric(range(colors))
+                        }
+                    } else {
+                        zlim <- c(-1,1)*as.numeric(quantile(abs(colors),p=gradient.range.quantile))
+                        if(diff(zlim)==0) {
+                            zlim <- c(-1,1)*as.numeric(max(abs(colors)))
+                        }
+                    }
+                }
+                                        # restrict the values
+                colors[colors<zlim[1]] <- zlim[1]; colors[colors>zlim[2]] <- zlim[2];
+
+                if(verbose) cat(' with zlim:',zlim,'\n')
+                colors <- (colors-zlim[1])/(zlim[2]-zlim[1])
+                cols <- gradientPalette[colors[match(rownames(emb),names(colors))]*(length(gradientPalette)-1)+1]
+            } else {
+                stop("colors argument must be a cell-named vector of either character colors or numeric values to be mapped to a gradient")
+            }
+        }
+    } else {
+        if(!is.null(groups)) {
+            if(min.group.size>1) { groups[groups %in% levels(groups)[unlist(tapply(groups,groups,length))<min.group.size]] <- NA; groups <- droplevels(groups); }
+            groups <- as.factor(groups)[rownames(emb)]
+            if(verbose) cat("using provided groups as a factor\n")
+            factor.mapping=TRUE;
+            ## set up a rainbow color on the factor
+            factor.colors <- fac2col(groups,s=s,v=v,shuffle=shuffle.colors,min.group.size=min.group.size,unclassified.cell.color=unclassified.cell.color,level.colors=group.level.colors,return.details=T)
+            cols <- factor.colors$colors;
+        }
+    }
+    names(cols) <- rownames(emb)
+
+    if(do.par) {
+        par(mar = c(0.5,0.5,2.0,0.5), mgp = c(2,0.65,0), cex = 1.0);
+    }
+    plot(emb,col=adjustcolor(cols,alpha=alpha),cex=cex,pch=19,axes=F, ...); box();
+    if(mark.clusters) {
+        if(!is.null(groups)) {
+            cent.pos <- do.call(rbind,tapply(1:nrow(emb),groups,function(ii) apply(emb[ii,,drop=F],2,median)))
+            cent.pos <- na.omit(cent.pos);
+            text(cent.pos[,1],cent.pos[,2],labels=rownames(cent.pos),cex=mark.cluster.cex)
+        }
+    }
+    if(show.legend) {
+        if(factor.mapping) {
+            legend(x=legend.x,pch=rep(19,length(levels(groups))),bty='n',col=factor.colors$palette,legend=names(factor.colors$palette))
+        }
+    }
+}
+## a utility function to translate factor into colors
+fac2col <- function(x,s=1,v=1,shuffle=FALSE,min.group.size=1,return.details=F,unclassified.cell.color='gray50',level.colors=NULL) {
+    x <- as.factor(x);
+    if(min.group.size>1) {
+        x <- factor(x,exclude=levels(x)[unlist(tapply(rep(1,length(x)),x,length))<min.group.size])
+        x <- droplevels(x)
+    }
+    if(is.null(level.colors)) {
+        col <- rainbow(length(levels(x)),s=s,v=v);
+    } else {
+        col <- level.colors[1:length(levels(x))];
+    }
+    names(col) <- levels(x);
+
+    if(shuffle) col <- sample(col);
+
+    y <- col[as.integer(x)]; names(y) <- names(x);
+    y[is.na(y)] <- unclassified.cell.color;
+    if(return.details) {
+        return(list(colors=y,palette=col))
+    } else {
+        return(y);
+    }
+}
+## quick utility to check if given character vector is colors
+## thanks, Josh O'Brien: http://stackoverflow.com/questions/13289009/check-if-character-string-is-a-valid-color-representation
+areColors <- function(x) {
+    is.character(x) & sapply(x, function(X) {tryCatch(is.matrix(col2rgb(X)), error = function(e) FALSE)})
+}
+
+
+
+
+
+
+
